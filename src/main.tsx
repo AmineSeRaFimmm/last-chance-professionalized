15be4f90944ea9e9949019d4dac5f9dbf2df7ed7
AmineSeRaFimmm <eduardowgx@gmail.com>
2026-06-22 10:51:41 +0800

    Install projection wheel interaction

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Root from "./Root";
 import { installNumericInputGuard } from "./core/numericInputGuard";
+import { installProjectionWheel } from "./core/projectionWheel";
 import "./styles/global.css";
 import "./styles/progress.css";
 import "./styles/navigation.css";
 import "./styles/projection.css";
 
 installNumericInputGuard();
+installProjectionWheel();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
